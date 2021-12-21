ba03a21c26e78b1f664c27bf063471193a67ecad
开源oschina <115889198@qq.com>
2021-12-21 10:43:29 +0800

    spring定时器2次注入

--- src/main/java/com/fc/v2/common/quartz/QuartzConfig.java
+++ src/main/java/com/fc/v2/common/quartz/QuartzConfig.java
@@ -4,7 +4,6 @@ import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
 import org.quartz.SchedulerFactory;
 import org.quartz.impl.StdSchedulerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
